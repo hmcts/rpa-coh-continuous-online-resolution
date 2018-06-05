e7db0960fe7b00af7f75c51af59f57723e731af6
Jake <jake.huggins@solirius.com>
2018-06-05 14:37:10 +0100

    Add controller for creating onlinehearing

--- src/main/java/uk/gov/hmcts/reform/coh/controller/OnlineHearingController.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/OnlineHearingController.java
@@ -1,15 +1,24 @@
 package uk.gov.hmcts.reform.coh.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
 
 @RestController
-@RequestMapping("/online-hearing")
+@RequestMapping("/online-hearings")
 public class OnlineHearingController {
 
     @RequestMapping("/retrieve")
     public String retrieve() {
         return "Greetings from Online Hearing Controller";
     }
-    
+
+    @PostMapping(value = "")
+    public ResponseEntity<OnlineHearing> createOnlineHearing(@RequestBody OnlineHearing body) {
+        return ResponseEntity.ok(body);
+    }
+
 }
