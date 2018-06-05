4fbbc657fdfdcd752b4094ab7faf0d500f0ecdc3
Rose Azad Khan <rose.azadkhan@solirius.com>
2018-06-05 17:13:15 +0100

    remove unused import statements

--- src/test/java/uk/gov/hmcts/reform/coh/service/OnlineHearingServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/coh/service/OnlineHearingServiceTest.java
@@ -3,16 +3,11 @@ package uk.gov.hmcts.reform.coh.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Repository;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import uk.gov.hmcts.reform.coh.Application;
 import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
 import uk.gov.hmcts.reform.coh.repository.OnlineHearingRepository;
 
