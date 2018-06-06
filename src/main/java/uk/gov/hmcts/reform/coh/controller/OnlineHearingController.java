bdc2a12c35dc3adddb411337f3eb276b6c816cda
Jake Huggins <jake.huggins@solirius.com>
2018-06-06 10:15:20 +0000

    Merged in feature/coh-create_online_hearing_controller (pull request #2)
    Feature/coh create online hearing controller
    
    * Add controller for creating onlinehearing
    
    * add controller test
    
    * remove placeholder method
    
    * remove unused imports

--- src/main/java/uk/gov/hmcts/reform/coh/controller/OnlineHearingController.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/OnlineHearingController.java
@@ -1,15 +1,20 @@
 package uk.gov.hmcts.reform.coh.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
 
 @RestController
-@RequestMapping("/online-hearing")
+@RequestMapping("/online-hearings")
 public class OnlineHearingController {
 
-    @RequestMapping("/retrieve")
-    public String retrieve() {
-        return "Greetings from Online Hearing Controller";
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<OnlineHearing> createOnlineHearing(@RequestBody OnlineHearing body) {
+        return ResponseEntity.ok(body);
     }
-    
+
 }
