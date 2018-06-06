9a8e8ef9c3798cefe1692236398823134e595844
Phillip Denness <dennessphillip@gmail.com>
2018-06-06 09:53:23 +0100

    Clean up application.class

--- src/main/java/uk/gov/hmcts/reform/coh/Application.java
+++ src/main/java/uk/gov/hmcts/reform/coh/Application.java
@@ -1,19 +1,7 @@
 package uk.gov.hmcts.reform.coh;
 
-import java.util.Arrays;
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.transaction.annotation.Transactional;
-import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
-import uk.gov.hmcts.reform.coh.repository.OnlineHearingRepository;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Application {
@@ -21,11 +9,5 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @Autowired
-    private OnlineHearingRepository onlineHearingRepository;
-
-    @Autowired
-    private DataSource dataSource;
 
 }
