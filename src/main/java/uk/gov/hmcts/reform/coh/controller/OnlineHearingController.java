2db8b3de685cd49b96b72fba42e058060e6c17da
Phillip Denness <dennessphillip@gmail.com>
2018-06-06 12:13:37 +0100

    Initial changes

--- src/main/java/uk/gov/hmcts/reform/coh/controller/OnlineHearingController.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/OnlineHearingController.java
@@ -1,25 +1,42 @@
 package uk.gov.hmcts.reform.coh.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
+import uk.gov.hmcts.reform.coh.service.OnlineHearingService;
 
 @RestController
 @RequestMapping("/online-hearings")
 public class OnlineHearingController {
 
+    @Autowired
+    OnlineHearingService onlineHearingService;
+
     @RequestMapping("/retrieve")
-    public String retrieve() {
-        return "Greetings from Online Hearing Controller";
+    public ResponseEntity<OnlineHearing> retrieveOnlineHearing(@RequestBody OnlineHearing body) {
+
+        OnlineHearing onlineHearing = new OnlineHearing();
+        onlineHearing.setExternalRef(body.getExternalRef());
+        OnlineHearing retrievedOnlineHearing = onlineHearingService.retrieveOnlineHearingByExternalRef(onlineHearing);
+
+        return new ResponseEntity<OnlineHearing>(retrievedOnlineHearing, HttpStatus.OK);
     }
 
-    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+
+    @PostMapping(value = "/create", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<OnlineHearing> createOnlineHearing(@RequestBody OnlineHearing body) {
-        return ResponseEntity.ok(body);
-    }
 
+        OnlineHearing onlineHearing = new OnlineHearing();
+        onlineHearing.setExternalRef(body.getExternalRef());
+        OnlineHearing createdOnlineHearing = onlineHearingService.createOnlineHearing(onlineHearing);
+
+        return new ResponseEntity<OnlineHearing>(createdOnlineHearing, HttpStatus.OK);
+    }
 }
