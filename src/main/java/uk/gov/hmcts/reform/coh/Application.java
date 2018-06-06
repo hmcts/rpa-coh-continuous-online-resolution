7b6a6c010919dd27a434cc94b3548e5fb115edc0
Phillip Denness <dennessphillip@gmail.com>
2018-06-06 09:26:50 +0100

    Change port to default
    Remove debugging from application

--- src/main/java/uk/gov/hmcts/reform/coh/Application.java
+++ src/main/java/uk/gov/hmcts/reform/coh/Application.java
@@ -28,34 +28,4 @@ public class Application {
     @Autowired
     private DataSource dataSource;
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-            System.out.println("DATASOURCE = " + dataSource.toString() + "Connection" + dataSource.getConnection());
-
-            System.out.println("Saving....");
-            OnlineHearing onlineHearing = new OnlineHearing();
-            onlineHearing.setExternalRef("4539000034");
-            OnlineHearing savedOnlineHearing = onlineHearingRepository.save(onlineHearing);
-            System.out.println("Saved!");
-
-            System.out.println("\n1.findAll()...");
-
-            for (OnlineHearing onlineHearingOne : onlineHearingRepository.findAll()) {
-                System.out.println(onlineHearingOne.toString());
-            }
-
-            System.out.println("Done!");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
-
 }
