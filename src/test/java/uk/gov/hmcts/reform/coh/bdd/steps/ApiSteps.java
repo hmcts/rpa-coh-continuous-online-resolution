670cc6d5e038714f0c2b00d09a69da32f8435fee
Rose Azad Khan <rose.azadkhan@solirius.com>
2018-06-11 16:42:54 +0100

    Add api steps for online hearing cucumber test
    Added methods for steps in feature file

--- src/test/java/uk/gov/hmcts/reform/coh/bdd/steps/ApiSteps.java
+++ src/test/java/uk/gov/hmcts/reform/coh/bdd/steps/ApiSteps.java
@@ -1,27 +1,67 @@
 package uk.gov.hmcts.reform.coh.bdd.steps;
 
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.http.entity.StringEntity;
+import sun.net.www.http.HttpClient;
 
 public class ApiSteps {
-    @Given("^'SSCS' prepare a json request$")
-    public void sscs_prepare_a_json_request() throws Throwable {
 
+    private JSONObject json;
+    private StringEntity params;
+
+    private HttpURLConnection http;
+
+    @Before
+    public void setup(){
+        try {
+            URL url = new URL("localhost:8080/online-hearings/create");
+            URLConnection connection = url.openConnection();
+            http = (HttpURLConnection) connection;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Given("^set the 'external_id' to 'test_externalId(\\d+)'$")
-    public void set_the_external_id_to_test_externalId(int arg1) throws Throwable {
+    @Given("^'SSCS' prepare a json request$")
+    public void sscs_prepare_a_json_request() throws Throwable {
+        json = new JSONObject();
+    }
 
+    @And("^set the 'externalRef' field to ' \"([^\"]*)\": '$")
+    public void setTheExternalRefFieldTo(String externalRef) throws Throwable {
+        json.put("externalRef", externalRef);
     }
 
     @When("^a post request is sent to '/online-hearings/create'$")
     public void a_post_request_is_sent_to_online_hearings_create() throws Throwable {
+        //http.setRequestMethod("POST");
+        params = new StringEntity(json.toString());
+        http.setDoOutput(true); // implicitly sets the request method to POST
+        http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
 
+        http.connect();
+        try(OutputStream os = http.getOutputStream()) {
+            os.write(params);
+        }
     }
 
     @Then("^the response contains the following text '\"([^\"]*)\": '$")
-    public void the_response_contains_the_following_text(String arg1) throws Throwable {
+    public void the_response_contains_the_following_text(String text) throws Throwable {
 
     }
+
 }
