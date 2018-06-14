ea7d11f814893f4ceda86eb35123ecfa93644f8f
Rose Azad Khan <rose.azadkhan@solirius.com>
2018-06-14 11:18:25 +0100

    cucumber test for retrieve online hearing

--- src/test/java/uk/gov/hmcts/reform/coh/bdd/steps/ApiSteps.java
+++ src/test/java/uk/gov/hmcts/reform/coh/bdd/steps/ApiSteps.java
@@ -1,11 +1,10 @@
 package uk.gov.hmcts.reform.coh.bdd.steps;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -13,49 +12,67 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONObject;
 
 import org.apache.http.entity.StringEntity;
-import org.springframework.http.HttpMethod;
+import org.junit.After;
+import org.springframework.beans.factory.annotation.Autowired;
+import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
+import uk.gov.hmcts.reform.coh.repository.OnlineHearingRepository;
+import uk.gov.hmcts.reform.coh.service.OnlineHearingService;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class ApiSteps {
 
+    @Autowired
+    private OnlineHearingService onlineHearingService;
+
+    @Autowired
+    private OnlineHearingRepository onlineHearingRepository;
+
     private JSONObject json;
+    private String baseUrl;
 
     private HttpPost request;
     private HttpResponse response;
     private String responseString;
     private CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
-//    @Before
-//    public void setup(){
-//        // figure out how to append urls fragments to a base url
-//
-//    }
+    private ArrayList<String> newObjects = new ArrayList<>();
 
-    // maybe collapse the below steps into a single step
-    @Given("^SSCS prepare a json request$")
-    public void sscs_prepare_a_json_request() {
-        request = new HttpPost("http://localhost:8080/online-hearings/create");
-        request.addHeader("content-type", "application/json");
-        System.out.println("HERE");
+    @Before
+    public void setup(){
+        baseUrl = "http://localhost:8080/online-hearings";
+    }
+
+    @After
+    public void cleanUp() {
+        for (String object : newObjects) {
+            OnlineHearing onlineHearing = new OnlineHearing();
+            onlineHearing.setExternalRef(object);
+            onlineHearingService.deleteOnlineHearingByExternalRef(onlineHearing);
+            OnlineHearing retrievedOnlineHearing = onlineHearingService.retrieveOnlineHearingByExternalRef(onlineHearing);
+            assertNull(retrievedOnlineHearing);
+        }
+    }
+
+    @Given("^SSCS prepare a json request with the ' \"([^\"]*)\"' field set to ' \"([^\"]*)\" '$")
+    public void sscs_prepare_a_json_request_with_the_field_set_to(String fieldName, String fieldInput) throws Throwable {
         json = new JSONObject();
+        json.put(fieldName, fieldInput);
+        newObjects.add(fieldInput);
     }
 
-    @Given("^set the 'externalRef' field to ' \"([^\"]*)\" '$")
-    public void setTheExternalRefFieldTo(String externalRef) throws Throwable {
-        json.put("externalRef", externalRef);
-    }
-
-    @When("^a post request is sent to /online-hearings/create$")
-    public void a_post_request_is_sent_to_online_hearings_create() throws Throwable {
+    @When("^a post request is sent to ' \"([^\"]*)\"'$")
+    public void a_post_request_is_sent_to(String endpoint) throws Throwable {
+        request = new HttpPost(baseUrl + endpoint);
+        request.addHeader("content-type", "application/json");
         StringEntity params = new StringEntity(json.toString());
         request.setEntity(params);
         response = httpClient.execute(request);
-
         responseString = new BasicResponseHandler().handleResponse(response);
     }
 
@@ -69,10 +86,7 @@ public class ApiSteps {
 
     @Then("^the response contains the following text '\"([^\"]*)\" '$")
     public void the_response_contains_the_following_text(String text) {
-        System.out.println(responseString);
         assertTrue(responseString.contains(text));
     }
-
-    // Remove added entities from the database in cleanup
 
 }
--- src/test/resources/cucumber/online-hearing.feature
+++ src/test/resources/cucumber/online-hearing.feature
@@ -1,8 +1,19 @@
 Feature: Online hearing
+
   Scenario: Create online hearing
-    Given SSCS prepare a json request
-    And set the 'externalRef' field to ' "CucumberExternalRefTest" '
-    When a post request is sent to /online-hearings/create
+    Given SSCS prepare a json request with the ' "externalRef"' field set to ' "CucumberExternalRefTestCreate" '
+    When a post request is sent to ' "/create"'
     Then the client receives a 200 status code 
     And the response contains the following text '"onlineHearingId" '
-    And the response contains the following text '"externalRef" '
+    And the response contains the following text '"CucumberExternalRefTestCreate" '
+
+   Scenario: Read online hearing
+     Given SSCS prepare a json request with the ' "externalRef"' field set to ' "CucumberExternalRefTestRetrieve" '
+     And a post request is sent to ' "/create"'
+     And the client receives a 200 status code
+     And the response contains the following text '"CucumberExternalRefTestRetrieve" '
+     And SSCS prepare a json request with the ' "externalRef"' field set to ' "CucumberExternalRefTestRetrieve" '
+     And a post request is sent to ' "/retrieve"'
+     Then the client receives a 200 status code
+     And the response contains the following text '"onlineHearingId" '
+     And the response contains the following text '"CucumberExternalRefTestRetrieve" '
