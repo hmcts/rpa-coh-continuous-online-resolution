754104b02079ff25217a7109ca130e4c99bb27b5
Jake <jake.huggins@solirius.com>
2018-06-15 14:45:10 +0100

    Add swagger annotations

--- src/main/java/uk/gov/hmcts/reform/coh/controller/QuestionController.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/QuestionController.java
@@ -1,14 +1,18 @@
 package uk.gov.hmcts.reform.coh.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
 import uk.gov.hmcts.reform.coh.domain.Question;
 import uk.gov.hmcts.reform.coh.service.QuestionService;
 
 @RestController
-@RequestMapping("/online-hearings")
+@RequestMapping("/online-hearings/{oh_id}")
 public class QuestionController {
 
     private QuestionService questionService;
@@ -19,17 +23,23 @@ public class QuestionController {
     }
 
 
-    @GetMapping("/{oh_id}/questions/{questionId}")
+    @ApiOperation("Get a question")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success", response = Question.class)})
+    @GetMapping("/questions/{questionId}")
     public ResponseEntity<Question> getQuestion(@PathVariable Integer oh_id, @PathVariable Integer questionId) {
         return ResponseEntity.ok(questionService.retrieveQuestionById(questionId));
     }
 
-    @PostMapping(value = "/{oh_id}/questions", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation("Add a new question")
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "Created", response = Question.class)})
+    @PostMapping(value = "/questions", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Question> createQuestion(@PathVariable Integer oh_id, @RequestBody Question body) {
         return ResponseEntity.ok(questionService.createQuestion(oh_id, body));
     }
 
-    @PatchMapping(value = "/{oh_id}/questions/{questionId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation("Edit a question")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success", response = Question.class)})
+    @PatchMapping(value = "/questions/{questionId}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Question> editQuestion(@PathVariable Integer questionId, @RequestBody Question body) {
         return ResponseEntity.ok(questionService.editQuestion(questionId, body));
     }
