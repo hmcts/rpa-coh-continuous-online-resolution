fae8b63e280c05f782ef1704e7e1a9fb3d83dc70
Rose Azad Khan <rose.azadkhan@solirius.com>
2018-06-18 13:17:18 +0100

    add tests for get question and edit question

--- src/test/java/uk/gov/hmcts/reform/coh/controller/QuestionControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/coh/controller/QuestionControllerTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.coh.controller;
 
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +22,49 @@ public class QuestionControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    @Before
+    public void setUp () throws Exception {
+        String json = "{\n" +
+                "\t\"onlineHearingId\": 4,\n" +
+                "\t\"externalRef\" : \"case_id_123\" \n" +
+                "}";
+
+        mvc.perform(MockMvcRequestBuilders.post("/online-hearings/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk());
+    }
+
     @Test
-    public void testCreateOnlineQuestion() throws Exception {
+    public void testAddQuestion() throws Exception {
         String json = "{\n" +
                 "\t\"oh_id\": 4,\n" +
                 "\t\"body\" : \"Test question text\" \n" +
+                "}";
+
+        mvc.perform(MockMvcRequestBuilders.post("/online-hearings/4/questions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetQuestion() throws Exception {
+        String json = "{\n" +
+                "\t\"oh_id\": 4,\n" +
+                "}";
+
+        mvc.perform(MockMvcRequestBuilders.post("/online-hearings/4/questions")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testEditQuestion() throws Exception {
+        String json = "{\n" +
+                "\t\"oh_id\": 4,\n" +
+                "\t\"body\" : \"Test edit question text\" \n" +
                 "}";
 
         mvc.perform(MockMvcRequestBuilders.post("/online-hearings/4/questions")
