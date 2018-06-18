cbad169a71cd570a24878504e3b416888482559b
Phillip Denness <dennessphillip@gmail.com>
2018-06-18 16:08:37 +0100

    Make unit test not depend on db

--- src/test/java/uk/gov/hmcts/reform/coh/controller/OnlineHearingControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/coh/controller/OnlineHearingControllerTest.java
@@ -1,19 +1,28 @@
 package uk.gov.hmcts.reform.coh.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
+import uk.gov.hmcts.reform.coh.service.OnlineHearingService;
 
 import java.io.File;
 import java.util.Objects;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -22,9 +31,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class OnlineHearingControllerTest {
 
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
+
+    @Mock
+    private OnlineHearingService onlineHearingService;
 
     private static final String ENDPOINT = "/online-hearings";
+
+    @InjectMocks
+    private OnlineHearingController onlineHearingController;
+
+    @Before
+    public void setup(){
+        mockMvc = MockMvcBuilders.standaloneSetup(onlineHearingController).build();
+        given(onlineHearingService.createOnlineHearing(any(OnlineHearing.class))).willReturn(new OnlineHearing());
+    }
 
     @Test
     public void testCreateOnlineHearingWithJsonFile() throws Exception {
@@ -37,14 +58,14 @@ public class OnlineHearingControllerTest {
 
         System.out.println("JSONSTRING" + jsonString);
 
-        this.mvc.perform(MockMvcRequestBuilders.post("/online-hearings/")
-                .contentType(MediaType.APPLICATION_JSON_UTF8).content(jsonString))
+        mockMvc.perform(MockMvcRequestBuilders.post("/online-hearings/")
+                .contentType(MediaType.APPLICATION_JSON).content(jsonString))
                 .andExpect(status().is2xxSuccessful());
     }
 
     @Test
     public void testReadOnlineHearingWithJsonFile() throws Exception {
-        this.mvc.perform(MockMvcRequestBuilders.get(ENDPOINT + "/case_id_123")
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(ENDPOINT + "/case_id_123")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(""))
                 .andExpect(status().isOk())
