fa232423c0711910986fc170427ab73c417ac6c0
Phillip Denness <dennessphillip@gmail.com>
2018-06-21 12:52:11 +0100

    Add swagger

--- src/main/java/uk/gov/hmcts/reform/coh/controller/QuestionRoundController.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/QuestionRoundController.java
@@ -1,5 +1,8 @@
 package uk.gov.hmcts.reform.coh.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.coh.controller.answer.AnswerResponse;
 import uk.gov.hmcts.reform.coh.domain.QuestionRound;
 import uk.gov.hmcts.reform.coh.service.QuestionRoundService;
 
@@ -20,12 +24,19 @@ public class QuestionRoundController {
 
     private QuestionRoundService questionRoundService;
 
-
     @Autowired
     public QuestionRoundController(QuestionRoundService questionRoundService) {
         this.questionRoundService = questionRoundService;
     }
 
+    @ApiOperation(value = "Issue Question Round", notes = "A GET request is used to notify the jurisdiction.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = AnswerResponse.class),
+            @ApiResponse(code = 401, message = "Unauthorised"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 422, message = "Validation error")
+    })
     @GetMapping("/questionrounds/{round_id}")
     public ResponseEntity<QuestionRound> issueQuestions(@PathVariable UUID round_id) {
 
