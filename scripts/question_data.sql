8464237f19209d534a2ce9c4b52576640a41d90d
Phillip Denness <dennessphillip@gmail.com>
2018-06-25 11:32:56 +0100

    Remove test data

--- scripts/question_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO online_hearing(online_hearing_id, external_ref, jurisdiction_id) VALUES ('d9248584-4aa5-4cb0-aba6-d2633ad5a375', 'e2e_test_oh', 1);
-INSERT INTO question(online_hearing_id, question_id, question_round_id, question_text, subject, question_state_id)
-    VALUES ('d9248584-4aa5-4cb0-aba6-d2633ad5a375', 2000, 1, 'foo', 'bar', 1);
