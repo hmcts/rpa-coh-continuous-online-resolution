5d625cdd71bee3547a57906b05789e64f0f0a598
Phillip Denness <dennessphillip@gmail.com>
2018-06-25 11:17:08 +0100

    Schema update for branch
    file: db.changelog-diff-20180625-1114.yaml contains all db changes added to this branch

--- scripts/jurisdiction_data.sql
+++ scripts/jurisdiction_data.sql
@@ -1,1 +1,1 @@
-insert into jurisdiction(jurisdiction_id, jurisdiction_name, url) values (1, 'SSCS', 'http://SSCS.endpoint');
+insert into jurisdiction(jurisdiction_id, jurisdiction_name, url) values (1, 'SSCS', 'http://localhost:8080/SSCS/notifications');
