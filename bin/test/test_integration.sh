d1d963c403a9f822fd09c6243e2f46443db37acb
John Ho <john.ho@solirius.com>
2018-07-02 16:38:04 +0100

    Removed unused docker files

--- bin/test/test_integration.sh
+++ bin/test/test_integration.sh
@@ -6,27 +6,15 @@ TEST_URL=http://localhost:4603
 
 ./gradlew installDist bootRepackage
 
-docker-compose -f ./docker/compose/docker-compose-dm.yml \
--f ./docker/compose/docker-compose-dm-ports.yml \
--f ./docker/compose/docker-compose-em.yml \
--f ./docker/compose/docker-compose-em-ports.yml \
--f ./docker/compose/docker-compose-idam.yml \
+docker-compose -f ./docker/compose/docker-compose-idam.yml \
 -f ./docker/compose/docker-compose-idam-ports.yml \
 down
 
-docker-compose -f ./docker/compose/docker-compose-dm.yml \
--f ./docker/compose/docker-compose-dm-ports.yml \
--f ./docker/compose/docker-compose-em.yml \
--f ./docker/compose/docker-compose-em-ports.yml \
--f ./docker/compose/docker-compose-idam.yml \
+docker-compose -f ./docker/compose/docker-compose-idam.yml \
 -f ./docker/compose/docker-compose-idam-ports.yml \
 pull
 
-docker-compose -f ./docker/compose/docker-compose-dm.yml \
--f ./docker/compose/docker-compose-dm-ports.yml \
--f ./docker/compose/docker-compose-em.yml \
--f ./docker/compose/docker-compose-em-ports.yml \
--f ./docker/compose/docker-compose-idam.yml \
+docker-compose -f ./docker/compose/docker-compose-idam.yml \
 -f ./docker/compose/docker-compose-idam-ports.yml \
 up -d --build
 
