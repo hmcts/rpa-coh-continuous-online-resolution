76611a1d578bc89b5c1026b0c06cef7087ba131c
Alec Doran-Twyford <alec@alectronic.co>
2018-07-03 14:24:23 +0100

    update executable scripts

--- buildrun-docker-base.sh
+++ buildrun-docker-base.sh
@@ -5,19 +5,13 @@ clear;
 #./gradlew installDist bootRepackage
 
 docker-compose -f ./docker/docker-compose-base.yml \
--f ./docker/docker-compose-base-ports.yml \
 -f ./docker/compose/docker-compose-idam.yml \
--f ./docker/compose/docker-compose-idam-ports.yml \
 down
 
 docker-compose -f ./docker/docker-compose-base.yml \
--f ./docker/docker-compose-base-ports.yml \
 -f ./docker/compose/docker-compose-idam.yml \
--f ./docker/compose/docker-compose-idam-ports.yml \
 pull
 
 docker-compose -f ./docker/docker-compose-base.yml \
--f ./docker/docker-compose-base-ports.yml \
 -f ./docker/compose/docker-compose-idam.yml \
--f ./docker/compose/docker-compose-idam-ports.yml \
 up --build
