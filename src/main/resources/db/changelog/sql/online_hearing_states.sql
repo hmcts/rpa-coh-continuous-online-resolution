dbd060eb97f1462717876e48caf77e86a81dca47
johnhosolirius <38843249+johnhosolirius@users.noreply.github.com>
2018-07-04 13:58:36 +0100

    Import static data on build (#37)
    * Auto import static data from Liquibase
    
    * Force static data inserts to be run always
    
    * Remove deprecated file

---
+++ src/main/resources/db/changelog/sql/online_hearing_states.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.online_hearing_state(online_hearing_state_id, state)
+	VALUES
+		(1, 'continuous_online_hearing_started'),
+		(2, 'continuous_online_hearing_questions_issued'),
+		(3, 'continuous_online_hearing_answers_sent');
