9e16e0c9bc76b8854672e7337c0535de9e351b62
Jake <jake.huggins@solirius.com>
2018-07-06 17:06:40 +0100

    conform to api v0.0.5

--- src/main/java/uk/gov/hmcts/reform/coh/controller/state/StateResponse.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/state/StateResponse.java
@@ -1,9 +1,13 @@
 package uk.gov.hmcts.reform.coh.controller.state;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class StateResponse {
 
+    @JsonProperty(value = "state_name")
     private String name;
 
+    @JsonProperty(value = "state_datetime")
     private String datetime;
 
     public String getDatetime() {
