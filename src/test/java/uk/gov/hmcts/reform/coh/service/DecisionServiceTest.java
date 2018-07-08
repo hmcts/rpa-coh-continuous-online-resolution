7165a3908e08004ab2eb30b5cfcdfc0f60e6db7f
John Ho <john.ho@solirius.com>
2018-07-08 14:50:40 +0100

    Unit tests for service classes

---
+++ src/test/java/uk/gov/hmcts/reform/coh/service/DecisionServiceTest.java
@@ -0,0 +1,68 @@
+package uk.gov.hmcts.reform.coh.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.coh.controller.decision.DecisionRequest;
+import uk.gov.hmcts.reform.coh.controller.decision.DecisionRequestMapper;
+import uk.gov.hmcts.reform.coh.domain.Decision;
+import uk.gov.hmcts.reform.coh.domain.DecisionState;
+import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
+import uk.gov.hmcts.reform.coh.domain.OnlineHearingState;
+import uk.gov.hmcts.reform.coh.repository.DecisionRepository;
+import uk.gov.hmcts.reform.coh.util.JsonUtils;
+
+import java.io.IOException;
+import java.util.*;
+
+import static java.util.UUID.randomUUID;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+public class DecisionServiceTest {
+
+    @Mock
+    private DecisionRepository decisionRepository;
+
+    @Mock
+    private DecisionStateService decisionStateService;
+
+    private DecisionService decisionService;
+
+    private Decision decision;
+
+    private Decision newDecision;
+
+    private DecisionState decisionState;
+
+    @Before
+    public void setup() throws IOException {
+        DecisionRequest request = (DecisionRequest) JsonUtils.toObjectFromTestName("decision/standard_decision", DecisionRequest.class);
+
+        decisionState = new DecisionState();
+        decisionState.setState("decision_drafted");
+        decisionService = new DecisionService(decisionRepository);
+
+        decision = new Decision();
+        DecisionRequestMapper.map(request, decision, decisionState);
+
+        newDecision = new Decision();
+        DecisionRequestMapper.map(request, newDecision, decisionState);
+        newDecision.setDecisionId(UUID.randomUUID());
+
+        given(decisionStateService.retrieveDecisionStateByState("decision_drafted")).willReturn(Optional.ofNullable(decisionState));
+    }
+
+    @Test
+    public void testCreateDecision() {
+        when(decisionRepository.save(decision)).thenReturn(newDecision);
+        assertEquals(newDecision, decisionService.createDecision(decision));
+    }
+}
