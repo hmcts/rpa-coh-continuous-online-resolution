6fe9e274161e7a82d4f9fc1849722d621670298a
John Ho <john.ho@solirius.com>
2018-07-10 13:20:53 +0100

    Checkpoint check in of a basic test

--- src/test/java/uk/gov/hmcts/reform/coh/controller/question/TestQuestionResponse.java
+++ src/test/java/uk/gov/hmcts/reform/coh/controller/question/TestQuestionResponse.java
@@ -1,25 +1,38 @@
 package uk.gov.hmcts.reform.coh.controller.question;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import uk.gov.hmcts.reform.coh.controller.answer.AnswerRequest;
+import uk.gov.hmcts.reform.coh.domain.Question;
+import uk.gov.hmcts.reform.coh.domain.QuestionState;
 import uk.gov.hmcts.reform.coh.util.JsonUtils;
+
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 
 public class TestQuestionResponse {
 
-    @Test
-    public void testMapping() throws Exception {
-        String json = JsonUtils.getJsonInput("question/standard_question_response");
+    private UUID questionUuid;
 
-        ObjectMapper mapper = new ObjectMapper();
-        QuestionResponse request = mapper.readValue(json, QuestionResponse.class);
-
-        assertEquals("How do you do your shopping?", request.getQuestionHeaderText());
-        assertEquals("DRAFTED", request.getCurrentState().getName());
-        assertEquals("2018-07-06 11:44:42.526", request.getCurrentState().getDatetime());
+    @Before
+    public void setup() {
+        questionUuid = UUID.randomUUID();
     }
 
+    @Test
+    public void testResponseMappings() {
+        QuestionState state = new QuestionState();
+        state.setState("foo");
+        Question question = new Question();
+        question.setQuestionId(questionUuid);
+        question.setQuestionRound(1);
+        question.setQuestionState(state);
+        QuestionResponse response = new QuestionResponse();
+        QuestionResponseMapper.map(question, response);
 
+        assertEquals(questionUuid.toString(), response.getQuestionId());
+        assertEquals("1", response.getQuestionRound());
+    }
 }
