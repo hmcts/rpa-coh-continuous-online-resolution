3eb8b4cb23a49adf681c92770b64a9300c58f971
Rose Azad Khan <rose.azadkhan@solirius.com>
2018-07-10 14:15:58 +0100

    add correct tagging

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,6 +9,7 @@ locals {
 
 module "app" {
   source = "git@github.com:hmcts/moj-module-webapp?ref=master"
+  common_tags = "${var.common_tags}"
   product = "${local.app_full_name}"
   location = "${var.location}"
   env = "${var.env}"
