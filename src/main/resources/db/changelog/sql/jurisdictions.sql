6e6aace794e2107172a91526ba3f9c8ba946955c
Phillip Denness <dennessphillip@gmail.com>
2018-07-12 16:18:56 +0100

    If azure DB then use AAT url to test harness

--- src/main/resources/db/changelog/sql/jurisdictions.sql
+++ src/main/resources/db/changelog/sql/jurisdictions.sql
@@ -1,3 +1,9 @@
 insert into jurisdiction(jurisdiction_id, jurisdiction_name, url, max_question_rounds)
- select 1, 'SSCS', 'http://localhost:8080/SSCS/notifications', 2
- where not exists (select 1 from public.jurisdiction where jurisdiction_id = 1);
+ select 1, 'SSCS', 'https://coh-cor-aat.service.core-compute-aat.internal:443/SSCS/notifications', 2
+ where (select count(datname) from pg_database where datname='azure_maintenance') = 1
+ AND not exists (select 1 from public.jurisdiction where jurisdiction_id = 1);
+
+insert into jurisdiction(jurisdiction_id, jurisdiction_name, url, max_question_rounds)
+select 1, 'SSCS', 'http://localhost:8080/SSCS/notifications', 2
+where (select count(datname) from pg_database where datname='azure_maintenance') = 0
+AND not exists (select 1 from public.jurisdiction where jurisdiction_id = 1);
