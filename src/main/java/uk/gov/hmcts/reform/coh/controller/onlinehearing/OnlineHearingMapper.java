164cef7ab1c002dd5f2590a55a4969cf4e3dd461
Jake <jake.huggins@solirius.com>
2018-07-16 12:25:08 +0100

    sort histories and add state date to onlinehearing response

--- src/main/java/uk/gov/hmcts/reform/coh/controller/onlinehearing/OnlineHearingMapper.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/onlinehearing/OnlineHearingMapper.java
@@ -15,5 +15,11 @@ public class OnlineHearingMapper {
                 .stream()
                 .map( p -> new OnlineHearingResponse.PanelMember(p.getFullName()))
                 .collect(Collectors.toList()));
+        response.getCurrentState().setName(onlineHearing.getOnlineHearingState().getState());
+        response.getCurrentState().setDatetime
+                (onlineHearing.getOnlineHearingStateHistories().stream().sorted(
+                    (a, b) -> (a.getDateOccurred().compareTo(b.getDateOccurred()))).collect(Collectors.toList()
+        ).get(onlineHearing.getOnlineHearingStateHistories().size()-1).getDateOccurred().toString());
+
     }
 }
