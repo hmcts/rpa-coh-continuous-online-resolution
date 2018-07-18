03aa7b242d06ca259416cf838ce5140d4b31f35b
Rose Azad Khan <rose.azadkhan@solirius.com>
2018-07-18 11:28:15 +0100

    add file for event controller

---
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/EventController.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.reform.coh.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/continuous-online-hearings")
+public class EventController {
+
+
+}
