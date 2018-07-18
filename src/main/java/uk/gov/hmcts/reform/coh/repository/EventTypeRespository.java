a4050d7d10e7ff3f17eeb2c550c115e7245e65b8
Rose <aquamooose@gmail.com>
2018-07-18 12:40:35 +0100

    Revert "Coh 88 create schema for event notification (#83)" (#85)
    This reverts commit a2d0d8cc8008856a447cceef51ebd5ad8b5ffe71.

--- src/main/java/uk/gov/hmcts/reform/coh/repository/EventTypeRespository.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.coh.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import uk.gov.hmcts.reform.coh.domain.EventType;
-
-@Repository
-public interface EventTypeRespository extends CrudRepository<EventType, Integer> {
-}
