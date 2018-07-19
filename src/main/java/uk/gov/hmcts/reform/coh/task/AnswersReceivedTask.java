4bef94ed056f49c41f5ed21007e6626d142513ed
John Ho <john.ho@solirius.com>
2018-07-19 18:55:56 +0100

    Added debugging to see why things are not working on AAT

--- src/main/java/uk/gov/hmcts/reform/coh/task/AnswersReceivedTask.java
+++ src/main/java/uk/gov/hmcts/reform/coh/task/AnswersReceivedTask.java
@@ -43,22 +43,26 @@ public class AnswersReceivedTask {
 
     public void execute(OnlineHearing onlineHearing) {
 
+        log.debug("AnswersReceivedTask.execute(). For online hearing: " + onlineHearing.getOnlineHearingId());
         Optional<List<Question>> questions = questionService.finaAllQuestionsByOnlineHearing(onlineHearing);
+        log.debug("AnswersReceivedTask.execute(). OnlineHearing has questions: " + (questions.isPresent() ? questions.get().size() : "0"));
         if (!questions.isPresent()) {
             return;
         }
 
         for (Question question : questions.get()) {
             if (!questionContainsAnAnswer(question)) {
+                log.debug("AnswersReceivedTask.execute(). Question " + question.getQuestionId() + " does not have a submitted answer");
                 return;
             }
         }
 
         // If we get this far, then all questions have been answered
         String nextState = OnlineHearingStates.ANSWERS_SENT.getStateName();
+        log.debug("AnswersReceivedTask.execute(). Expected next state: " + nextState);
         Optional<OnlineHearingState> state = onlineHearingStateService.retrieveOnlineHearingStateByState(nextState);
         if (!state.isPresent()) {
-            log.debug("Failed to update online hearing after all questions have been answered. Unable to retrieve " + nextState);
+            log.debug("AnswersReceivedTask.execute(). Failed to update online hearing after all questions have been answered. Unable to retrieve " + nextState);
             return;
         }
 
@@ -73,6 +77,7 @@ public class AnswersReceivedTask {
         List<Answer> answers = answerService.retrieveAnswersByQuestion(question);
         if (!answers.isEmpty()) {
             // For MVP, there should only be one answer
+            log.debug("AnswersReceivedTask.questionContainsAnAnswer(). Status of answer " + answers.get(0).getAnswerId() + " is " + answers.get(0).getAnswerState().getState());
             return answers.get(0).getAnswerState().getState().equals(AnswerStates.SUBMITTED.getStateName());
         }
 
