e927e1de8eb5b4a90d1dd86f5c848ced9db46519
John Ho <john.ho@solirius.com>
2018-07-19 12:07:33 +0100

    Refactor to keep Sonar happy

--- src/main/java/uk/gov/hmcts/reform/coh/controller/validators/QuestionValidator.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/validators/QuestionValidator.java
@@ -4,10 +4,18 @@ import org.springframework.util.StringUtils;
 import uk.gov.hmcts.reform.coh.controller.question.QuestionRequest;
 import java.util.function.Predicate;
 
+import static uk.gov.hmcts.reform.coh.controller.validators.ValidatorUtils.isPositiveInteger;
+
 public enum QuestionValidator implements Validator<QuestionRequest>{
 
-    QUESTION_ROUND(qr -> StringUtils.isEmpty(qr.getQuestionRound()), "Question round is required"),
-    QUESTION_ORDINAL(qr -> StringUtils.isEmpty(qr.getQuestionOrdinal()), "Question ordinal is required"),
+    QUESTION_ROUND(qr -> {
+        return StringUtils.isEmpty(qr.getQuestionRound())
+                || !isPositiveInteger(qr.getQuestionRound());
+        }, "Question round is required and must be numeric"),
+    QUESTION_ORDINAL(qr -> {
+        return StringUtils.isEmpty(qr.getQuestionOrdinal())
+                && !isPositiveInteger(qr.getQuestionOrdinal());
+        }, "Question ordinal is required and must be numeric"),
     QUESTION_HEADER(qr -> StringUtils.isEmpty(qr.getQuestionHeaderText()), "Question header text is required"),
     QUESTION_BODY(qr -> StringUtils.isEmpty(qr.getQuestionBodyText()), "Question body text is required"),
     QUESTION_OWNER(qr -> StringUtils.isEmpty(qr.getOwnerReference()), "Owner reference is required");
---
+++ src/test/java/uk/gov/hmcts/reform/coh/controller/validators/QuestionRequestValidatorTest.java
@@ -0,0 +1,101 @@
+package uk.gov.hmcts.reform.coh.controller.validators;
+
+import org.junit.Before;
+import org.junit.Test;
+import uk.gov.hmcts.reform.coh.controller.question.QuestionRequest;
+import uk.gov.hmcts.reform.coh.util.JsonUtils;
+
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+public class QuestionRequestValidatorTest {
+
+    QuestionRequest request;
+
+    Validation validation;
+
+    @Before
+    public void setup() throws IOException {
+        request = (QuestionRequest)JsonUtils.toObjectFromTestName("question/standard_question", QuestionRequest.class);
+        validation = new Validation();
+    }
+
+    @Test
+    public void testValidator() {
+        assertTrue(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+
+    @Test
+    public void testNonNumericQuestionRound() {
+        request.setQuestionRound("abc");
+        assertFalse(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+
+    @Test
+    public void testNonPositiveQuestionRound() {
+        request.setQuestionRound("-1");
+        assertFalse(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+
+    @Test
+    public void testZeroQuestionRound() {
+        request.setQuestionRound("0");
+        assertTrue(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+
+    @Test
+    public void testNonNumericQuestionOrdinal() {
+        request.setQuestionRound("abc");
+        assertFalse(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+
+    @Test
+    public void testNonPositiveQuestionOrdinal() {
+        request.setQuestionRound("-1");
+        assertFalse(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+
+    @Test
+    public void testZeroQuestionOrdinal() {
+        request.setQuestionRound("0");
+        assertTrue(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+
+    @Test
+    public void testNullHeaderText() {
+        request.setQuestionHeaderText(null);
+        assertFalse(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+
+    @Test
+    public void testEmptyHeaderText() {
+        request.setQuestionHeaderText("");
+        assertFalse(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+
+    @Test
+    public void testNullQuestionBodyText() {
+        request.setQuestionBodyText(null);
+        assertFalse(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+
+    @Test
+    public void testEmptyQuestionBodyText() {
+        request.setQuestionBodyText("");
+        assertFalse(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+
+    @Test
+    public void testNulOwnerReference() {
+        request.setOwnerReference(null);
+        assertFalse(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+
+    @Test
+    public void testEmptyOwnerReference() {
+        request.setOwnerReference("");
+        assertFalse(validation.execute(QuestionValidator.values(), request).isValid());
+    }
+}
