cff92157d5a18d53b5d906ded070f34c3538e4ae
Jake <jake.huggins@solirius.com>
2018-07-23 17:20:26 +0100

    add resource not found exception

--- src/main/java/uk/gov/hmcts/reform/coh/service/EventTypeService.java
+++ src/main/java/uk/gov/hmcts/reform/coh/service/EventTypeService.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.coh.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.coh.controller.exceptions.ResourceNotFoundException;
 import uk.gov.hmcts.reform.coh.domain.EventType;
 import uk.gov.hmcts.reform.coh.repository.EventTypeRepository;
 
@@ -20,7 +21,14 @@ public class EventTypeService {
     }
 
     public Optional<EventType> retrieveEventType(String eventType){
-        return eventTypeRepository.findByEventTypeName(eventType);
+        Optional<EventType> event = eventTypeRepository.findByEventTypeName(eventType);
+
+        if (!event.isPresent()){
+            throw new ResourceNotFoundException("EventType Not Found");
+        }
+
+        return event;
+
     }
 
 }
