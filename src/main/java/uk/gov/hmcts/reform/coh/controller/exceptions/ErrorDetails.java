85158f3d4de622526ca4ef58d154c2492b3b9930
Jake <jake.huggins@solirius.com>
2018-07-24 13:27:07 +0100

    Test custom resourcenotfound exception

--- src/main/java/uk/gov/hmcts/reform/coh/controller/exceptions/ErrorDetails.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/exceptions/ErrorDetails.java
@@ -18,24 +18,13 @@ public class ErrorDetails {
         return timestamp;
     }
 
-    public void setTimestamp(Date timestamp) {
-        this.timestamp = timestamp;
-    }
-
     public String getMessage() {
         return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
     }
 
     public String getDetails() {
         return details;
     }
 
-    public void setDetails(String details) {
-        this.details = details;
-    }
 
 }
---
+++ src/test/java/uk/gov/hmcts/reform/coh/service/EventTypeServiceTest.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.reform.coh.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.coh.controller.exceptions.ResourceNotFoundException;
+import uk.gov.hmcts.reform.coh.repository.EventTypeRepository;
+import uk.gov.hmcts.reform.coh.repository.JurisdictionRepository;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+@RunWith(SpringRunner.class)
+public class EventTypeServiceTest {
+    @Mock
+    private EventTypeRepository eventTypeRepository;
+    @Mock
+    private EventTypeService eventTypeService;
+
+    @Before
+    public void setup() {
+        eventTypeService = new EventTypeService(eventTypeRepository);
+
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void testInvalidJurisdictionThrowsResourceNotFoundException() throws Exception {
+        eventTypeService.retrieveEventType("Chocolate");
+    }
+
+    @Test
+    public void testExceptionMessage() throws Exception {
+        try {
+            eventTypeService.retrieveEventType("Chocolate");
+        } catch (ResourceNotFoundException e){
+            assertThat(e.getMessage(), is("EventType Not Found"));
+        }
+    }
+
+}
