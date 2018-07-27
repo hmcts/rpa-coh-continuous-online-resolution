0dbd355845a4ba91a2d3d06160eae0fc6397a8c8
Phillip Denness <dennessphillip@gmail.com>
2018-07-27 16:08:43 +0100

    Fix deadline to be 7 days from midnight

--- src/main/java/uk/gov/hmcts/reform/coh/service/utils/ExpiryCalendar.java
+++ src/main/java/uk/gov/hmcts/reform/coh/service/utils/ExpiryCalendar.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.coh.service.utils;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class ExpiryCalendar {
     /**
@@ -10,9 +11,9 @@ public class ExpiryCalendar {
      * @return
      */
     public static Date getDeadlineExpiryDate() {
-        Calendar calendar = new GregorianCalendar();
+        Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
         calendar.add(Calendar.DAY_OF_YEAR, 7);
-        calendar.set(Calendar.HOUR, 23);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
         calendar.set(Calendar.MINUTE, 59);
         calendar.set(Calendar.SECOND, 59);
 
