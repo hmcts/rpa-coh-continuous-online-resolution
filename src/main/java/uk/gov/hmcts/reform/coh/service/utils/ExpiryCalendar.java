62f510747c3d2ecbb0c2b3406ba74995d719c807
Phillip Denness <dennessphillip@gmail.com>
2018-07-27 16:24:03 +0100

    Set microseconds to 0 for consistency.

--- src/main/java/uk/gov/hmcts/reform/coh/service/utils/ExpiryCalendar.java
+++ src/main/java/uk/gov/hmcts/reform/coh/service/utils/ExpiryCalendar.java
@@ -16,6 +16,7 @@ public class ExpiryCalendar {
         calendar.set(Calendar.HOUR_OF_DAY, 23);
         calendar.set(Calendar.MINUTE, 59);
         calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 0);
 
         return calendar.getTime();
     }
