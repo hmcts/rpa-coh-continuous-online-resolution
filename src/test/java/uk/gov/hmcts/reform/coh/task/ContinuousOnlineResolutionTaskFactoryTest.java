56960909f6503fd21742a1d33eeb6dc632d8dfdb
John Ho <john.ho@solirius.com>
2018-07-28 10:43:14 +0100

    Unused import

--- src/test/java/uk/gov/hmcts/reform/coh/task/ContinuousOnlineResolutionTaskFactoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/coh/task/ContinuousOnlineResolutionTaskFactoryTest.java
@@ -4,7 +4,6 @@ package uk.gov.hmcts.reform.coh.task;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
