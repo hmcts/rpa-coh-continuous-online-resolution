073423195e4b0abbd9a55880832ad095db206cb2
John Ho <john.ho@solirius.com>
2018-07-28 10:27:41 +0100

    Codacy fixes

--- src/test/java/uk/gov/hmcts/reform/coh/task/ContinuousOnlineResolutionTaskFactoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/coh/task/ContinuousOnlineResolutionTaskFactoryTest.java
@@ -7,12 +7,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.reform.coh.events.EventTypes;
 
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 @SpringBootTest
