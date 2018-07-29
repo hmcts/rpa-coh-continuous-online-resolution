9c450423f5b171ff23fe5ac0ff3c04a189067703
John Ho <john.ho@solirius.com>
2018-07-29 15:38:31 +0100

    Event trigger now retireved from factory

--- src/main/java/uk/gov/hmcts/reform/coh/schedule/trigger/EventTriggerJob.java
+++ src/main/java/uk/gov/hmcts/reform/coh/schedule/trigger/EventTriggerJob.java
@@ -1,17 +1,32 @@
 package uk.gov.hmcts.reform.coh.schedule.trigger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 @Component
 public class EventTriggerJob {
+
+    private static final Logger log = LoggerFactory.getLogger(EventTriggerJob.class);
 
     @Autowired
     private QuestionRoundDeadlineElapsed trigger;
 
+    @Autowired
+    private EventTriggerFactory factory;
+
     @Scheduled(fixedDelayString  = "${event-scheduler.event-trigger.fixed-delay}")
     public void execute() {
-        trigger.execute();
+
+        log.info("Running " + this.getClass());
+        Set<EventTrigger> triggers = factory.getTriggers();
+        for (EventTrigger trigger : triggers) {
+            log.info(String.format("Running event trigger %s", trigger.getClass()));
+            trigger.execute();
+        }
     }
 }
