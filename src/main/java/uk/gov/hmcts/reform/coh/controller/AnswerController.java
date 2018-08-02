301777ae3e70ae5d2ac82c4554fd505238cb7882
John Ho <john.ho@solirius.com>
2018-08-02 09:26:29 +0100

    Better error message if trying to answer question not issued yet

--- src/main/java/uk/gov/hmcts/reform/coh/controller/AnswerController.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/AnswerController.java
@@ -93,7 +93,7 @@ public class AnswerController {
             // If a question exists, then it must be in the issued state to be answered
             if (!optionalQuestion.isPresent()
                     || !optionalQuestion.get().getQuestionState().getState().equals(QuestionStates.ISSUED.getStateName())) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The question does not exist");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The question does not exist or not yet issued");
             }
 
             // For MVP, there'll only be one answer per question
