4d49338d0769272c5882461cacb6baba0fe6f6e4
Rose Azad Khan <rose.azadkhan@solirius.com>
2018-08-06 17:18:08 +0100

    add transformer for question_deadline_extension_requested

---
+++ src/main/java/uk/gov/hmcts/reform/coh/schedule/notifiers/QuestionDeadlineExtensionRequestedTransformer.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.reform.coh.schedule.notifiers;
+
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import org.springframework.beans.factory.annotation.Autowired;
+import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
+import uk.gov.hmcts.reform.coh.domain.Question;
+import uk.gov.hmcts.reform.coh.domain.SessionEventType;
+import uk.gov.hmcts.reform.coh.service.QuestionRoundService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static uk.gov.hmcts.reform.coh.events.EventTypes.QUESTION_DEADLINE_EXTENDED;
+import static uk.gov.hmcts.reform.coh.events.EventTypes.QUESTION_ROUND_ISSUED;
+
+public class QuestionDeadlineExtensionRequestedTransformer implements EventTransformer<OnlineHearing> {
+
+    private static final ISO8601DateFormat df = new ISO8601DateFormat();
+
+    @Autowired
+    private QuestionRoundService questionRoundService;
+
+    @Override
+    public NotificationRequest transform(SessionEventType sessionEventType, OnlineHearing onlineHearing) {
+        List<Question> qrQuestions = questionRoundService.getQuestionsByQuestionRound(onlineHearing, questionRoundService.getCurrentQuestionRoundNumber(onlineHearing));
+
+        NotificationRequest request = new NotificationRequest();
+        request.setCaseId(onlineHearing.getCaseId());
+        request.setOnlineHearingId(onlineHearing.getOnlineHearingId());
+        request.setEventType(QUESTION_DEADLINE_EXTENDED.getEventType());
+        request.setExpiryDate(df.format(qrQuestions.get(0).getDeadlineExpiryDate()));
+        return request;
+    }
+
+    @Override
+    public List<String> supports() {
+        return Arrays.asList(
+                QUESTION_ROUND_ISSUED.getEventType()
+        );
+    }
+
+
+}
--- src/main/java/uk/gov/hmcts/reform/coh/states/QuestionStates.java
+++ src/main/java/uk/gov/hmcts/reform/coh/states/QuestionStates.java
@@ -6,6 +6,7 @@ public enum QuestionStates {
     ISSUE_PENDING("question_issue_pending"),
     ISSUED("question_issued"),
     DEADLINE_ELAPSED("question_deadline_elapsed"),
+    QUESTION_EXTENSION_REQUESTED("question_extension_requested"),
     QUESTION_DEADLINE_EXTENSION_DENIED("question_deadline_extension_denied"),
     QUESTION_DEADLINE_EXTENSION_GRANTED("question_deadline_extension_granted");
 
