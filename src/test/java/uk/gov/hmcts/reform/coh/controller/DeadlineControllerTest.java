5f0bf8ff670356abce7addfad4f490effb2c8545
John Ho <john.ho@solirius.com>
2018-08-07 17:58:11 +0100

    Improved unit test coverage

--- src/test/java/uk/gov/hmcts/reform/coh/controller/DeadlineControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/coh/controller/DeadlineControllerTest.java
@@ -1,7 +1,9 @@
 package uk.gov.hmcts.reform.coh.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,18 +11,19 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import uk.gov.hmcts.reform.coh.controller.state.DeadlineExtensionHelper;
 import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
+import uk.gov.hmcts.reform.coh.events.EventTypes;
 import uk.gov.hmcts.reform.coh.service.OnlineHearingService;
 import uk.gov.hmcts.reform.coh.service.QuestionService;
+import uk.gov.hmcts.reform.coh.service.SessionEventService;
 import uk.gov.hmcts.reform.coh.service.exceptions.NoQuestionsAsked;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -36,8 +39,68 @@ public class DeadlineControllerTest {
     @MockBean
     private QuestionService questionService;
 
+    @MockBean
+    private SessionEventService sessionEventService;
+
     @Autowired
     private MockMvc mockMvc;
+
+    private DeadlineExtensionHelper helper;
+
+    @Before
+    public void setUp() throws NoQuestionsAsked {
+        helper = new DeadlineExtensionHelper(0, 0, 0);
+        when(questionService.requestDeadlineExtension(any())).thenReturn(helper);
+    }
+
+    @Test
+    public void testOnlineHearingNotFound() throws Exception {
+        when(onlineHearingService.retrieveOnlineHearing(any(OnlineHearing.class))).thenReturn(Optional.empty());
+
+        UUID onlineHearingId = UUID.randomUUID();
+        mockMvc.perform(put("/continuous-online-hearings/" + onlineHearingId + "/questions-deadline-extension"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testNoGrantedNoDenied() throws Exception {
+        helper = new DeadlineExtensionHelper(1, 0, 0);
+        OnlineHearing spyOnlineHearing = spy(OnlineHearing.class);
+        Optional<OnlineHearing> onlineHearing = Optional.of(spyOnlineHearing);
+        when(onlineHearingService.retrieveOnlineHearing(any(OnlineHearing.class))).thenReturn(onlineHearing);
+
+        UUID onlineHearingId = UUID.randomUUID();
+        mockMvc.perform(put("/continuous-online-hearings/" + onlineHearingId + "/questions-deadline-extension"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void tesDeniedAndSessionEventQueued() throws Throwable {
+        helper = new DeadlineExtensionHelper(1, 0, 1);
+        when(questionService.requestDeadlineExtension(any())).thenReturn(helper);
+        OnlineHearing spyOnlineHearing = spy(OnlineHearing.class);
+        Optional<OnlineHearing> onlineHearing = Optional.of(spyOnlineHearing);
+        when(onlineHearingService.retrieveOnlineHearing(any(OnlineHearing.class))).thenReturn(onlineHearing);
+
+        UUID onlineHearingId = UUID.randomUUID();
+        mockMvc.perform(put("/continuous-online-hearings/" + onlineHearingId + "/questions-deadline-extension"))
+                .andExpect(status().isOk());
+        verify(sessionEventService, times(1)).createSessionEvent(onlineHearing.get(), EventTypes.QUESTION_DEADLINE_EXTENSION_DENIED.getEventType());
+    }
+
+    @Test
+    public void testGrantedAndSessionEventQueued() throws Throwable {
+        helper = new DeadlineExtensionHelper(1, 1, 0);
+        when(questionService.requestDeadlineExtension(any())).thenReturn(helper);
+        OnlineHearing spyOnlineHearing = spy(OnlineHearing.class);
+        Optional<OnlineHearing> onlineHearing = Optional.of(spyOnlineHearing);
+        when(onlineHearingService.retrieveOnlineHearing(any(OnlineHearing.class))).thenReturn(onlineHearing);
+
+        UUID onlineHearingId = UUID.randomUUID();
+        mockMvc.perform(put("/continuous-online-hearings/" + onlineHearingId + "/questions-deadline-extension"))
+                .andExpect(status().isOk());
+        verify(sessionEventService, times(1)).createSessionEvent(onlineHearing.get(), EventTypes.QUESTION_DEADLINE_EXTENSION_GRANTED.getEventType());
+    }
 
     @Test
     public void testSuccessfulExtensionRequest() throws Exception {
