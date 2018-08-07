bcd0a3985fd61ff42b571dd5f1f4082158aa3904
John Ho <john.ho@solirius.com>
2018-08-07 18:06:49 +0100

    Cadacy compliantsexit

--- src/test/java/uk/gov/hmcts/reform/coh/controller/DeadlineControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/coh/controller/DeadlineControllerTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.coh.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
