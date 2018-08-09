3f37827563affc2779aa40513cfa1afd0118c068
John Ho <john.ho@solirius.com>
2018-08-09 09:14:12 +0100

    Exception no needed

--- src/main/java/uk/gov/hmcts/reform/coh/service/exceptions/NoQuestionsAsked.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.hmcts.reform.coh.service.exceptions;
-
-public class NoQuestionsAsked extends Exception {
-    public NoQuestionsAsked(String message) {
-        super(message);
-    }
-}
