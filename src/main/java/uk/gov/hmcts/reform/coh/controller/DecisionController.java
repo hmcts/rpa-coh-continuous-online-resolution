153d7d1378c118cca1184427af59b90143c837ab
John Ho <john.ho@solirius.com>
2018-08-09 16:11:29 +0100

    Quick fix

--- src/main/java/uk/gov/hmcts/reform/coh/controller/DecisionController.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/DecisionController.java
@@ -105,7 +105,7 @@ public class DecisionController {
         Decision decision = new Decision();
         decision.setOnlineHearing(optionalOnlineHearing.get());
         DecisionRequestMapper.map(request, decision, optionalDecisionState.get());
-        decision.setAuthorReferenceId(authorReferenceId);
+        //decision.setAuthorReferenceId(authorReferenceId);
         decision.addDecisionStateHistory(optionalDecisionState.get());
         decision = decisionService.createDecision(decision);
 
