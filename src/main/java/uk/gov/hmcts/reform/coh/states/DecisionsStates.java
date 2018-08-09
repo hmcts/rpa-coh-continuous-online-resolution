48661f034413cfed9cf517d188629e04cb5c51fc
Rose Azad Khan <rose.azadkhan@solirius.com>
2018-08-09 12:31:30 +0100

    add deleted file

---
+++ src/main/java/uk/gov/hmcts/reform/coh/states/DecisionsStates.java
@@ -0,0 +1,20 @@
+package uk.gov.hmcts.reform.coh.states;
+
+public enum DecisionsStates {
+
+    DECISION_DRAFTED("decision_drafted"),
+    DECISION_ISSUE_PENDING("decision_issue_pending"),
+    DECISION_ISSUED("decision_issued"),
+    DECISIONS_REJECTED("decision_rejected"),
+    DECISIONS_ACCEPTED("decision_accepted");
+
+    private String stateName;
+
+    DecisionsStates(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+}
