3c4a83fc698c607aa5e5caa790cc1d8234c61d5e
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-08-10 14:28:37 +0100

    Annotate AnswerRequest

--- src/main/java/uk/gov/hmcts/reform/coh/controller/answer/AnswerRequest.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/answer/AnswerRequest.java
@@ -1,12 +1,15 @@
 package uk.gov.hmcts.reform.coh.controller.answer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 
 public class AnswerRequest {
 
+    @ApiModelProperty(required = true)
     @JsonProperty("answer_text")
     private String answerText;
 
+    @ApiModelProperty(required = true, allowableValues = "answer_drafted, answer_submitted")
     @JsonProperty("answer_state")
     private String answerState;
 
