d767f639624a7c8147ceae5f968d16ccd2fe905a
Phillip Denness <dennessphillip@gmail.com>
2018-08-10 10:29:36 +0100

    Create SSCS with unlimited QR

--- src/main/resources/db/changelog/sql/jurisdictions.sql
+++ src/main/resources/db/changelog/sql/jurisdictions.sql
@@ -1,3 +1,3 @@
 insert into jurisdiction(jurisdiction_id, jurisdiction_name, max_question_rounds)
- select 1, 'SSCS', 2
+ select 1, 'SSCS', 0
  where not exists (select 1 from public.jurisdiction where jurisdiction_id = 1);
