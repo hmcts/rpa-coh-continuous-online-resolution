e3116f32323fcbfebf02a121119dd1c6c453d605
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-08-10 14:32:25 +0100

    Annotate DecisionReplyRequest

--- src/main/java/uk/gov/hmcts/reform/coh/controller/decisionreplies/DecisionReplyRequest.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/decisionreplies/DecisionReplyRequest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.coh.controller.decisionreplies;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -10,11 +11,13 @@ public class DecisionReplyRequest {
     @JsonProperty("decision_reply")
     @NotNull(message = "Decision reply required")
     @NotBlank(message = "Decision reply required")
+    @ApiModelProperty(required = true)
     private String decisionReply;
 
     @JsonProperty("decision_reply_reason")
     @NotNull(message = "Decision reply reason required")
     @NotBlank(message = "Decision reply reason required")
+    @ApiModelProperty(required = true)
     private String decisionReplyReason;
 
 
