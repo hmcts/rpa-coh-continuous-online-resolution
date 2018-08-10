a966f54c30fab743afd240e2536c7e607004ba08
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-08-10 09:34:03 +0100

    Enable checking IdAM and S2S tokens
    SecurityConfiguration states that any request should be authenticated
    using AuthCheckerServiceAndUserFilter.
    
    SecurityClientConfiguration is responsible for configuring
    pre-authentication processing filter (AuthCheckedServiceAndUserFilter)
    with all its dependencies.
    
    application.yaml has new entries for configuring roles and services that
    are allowed to use the app, and URLs for IdAM and S2S servers.
    
    The application can be also configured with environment variables:
    
    - AUTHORIZATION_ROLES
      Comma-separated list of IdAM user's roles that are allowed to use the
      app; default: "judge"
    
    - S2S_NAMES_WHITELIST
      Comma-separated list of S2S service identifiers that are allowed to
      use the app; default: "sscs"
    
    - IDAM_USER_BASE_URI
      URL to a running IdAM instance; default: http://localhost:4501
    
    - IDAM_S2S_BASE_URI
      URL to a running S2S instance; default: http://localhost:4502

---
+++ src/main/java/uk/gov/hmcts/reform/coh/config/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package uk.gov.hmcts.reform.coh.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final AbstractPreAuthenticatedProcessingFilter filter;
+
+    @Autowired
+    public SecurityConfiguration(AbstractPreAuthenticatedProcessingFilter filter) {
+        super();
+        this.filter = filter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .addFilter(filter)
+            .authorizeRequests()
+            .anyRequest()
+            .authenticated();
+    }
+}
