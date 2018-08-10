c7320ab4660405cebfb4c469dbceae1fbe1e2df3
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-08-10 15:48:09 +0100

    Annotate QuestionRoundRequest

--- src/main/java/uk/gov/hmcts/reform/coh/controller/questionrounds/QuestionRoundRequest.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/questionrounds/QuestionRoundRequest.java
@@ -1,10 +1,12 @@
 package uk.gov.hmcts.reform.coh.controller.questionrounds;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 
 public class QuestionRoundRequest {
 
     @JsonProperty("state_name")
+    @ApiModelProperty(required = true, allowableValues = "question_issue_pending")
     private String stateName;
 
     public String getStateName() {
