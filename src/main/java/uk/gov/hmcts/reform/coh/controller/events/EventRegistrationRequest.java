0f0e3ebfa6594c858be03c4e1de5a5540b6010d6
John Ho <john.ho@solirius.com>
2018-08-13 11:04:05 +0100

    Codacy complaints

--- src/main/java/uk/gov/hmcts/reform/coh/controller/events/EventRegistrationRequest.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/events/EventRegistrationRequest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.validator.constraints.URL;
-import uk.gov.hmcts.reform.coh.events.EventTypes;
 
 import javax.validation.constraints.*;
 
