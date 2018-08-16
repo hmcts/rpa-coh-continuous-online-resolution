70c90eb88beda68d5288092f6cc0c737fbd9ef01
Phillip Denness <dennessphillip@gmail.com>
2018-08-16 09:09:37 +0100

    Apply to continuous-online-hearing paths (#166)

--- src/main/java/uk/gov/hmcts/reform/coh/config/WebConfig.java
+++ src/main/java/uk/gov/hmcts/reform/coh/config/WebConfig.java
@@ -1,19 +1,20 @@
 package uk.gov.hmcts.reform.coh.config; 
  
 import org.springframework.beans.factory.annotation.Autowired; 
-import org.springframework.context.annotation.Configuration; 
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry; 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter; 
 import uk.gov.hmcts.reform.coh.handlers.IdamHeaderInterceptor;
 
-@Configuration 
-public class WebMvcConfig extends WebMvcConfigurerAdapter { 
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
  
     @Autowired 
     IdamHeaderInterceptor idamHeaderInterceptor; 
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) { 
-        registry.addInterceptor(idamHeaderInterceptor); 
+        registry.addInterceptor(idamHeaderInterceptor).addPathPatterns("/continuous-online-hearings/**");
     } 
 } 
