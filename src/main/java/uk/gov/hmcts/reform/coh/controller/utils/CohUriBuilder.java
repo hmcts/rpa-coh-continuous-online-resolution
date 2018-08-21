53224febbe7603b4e6f77598a4d71705e9b23787
John Ho <john.ho@solirius.com>
2018-08-21 10:22:05 +0100

    Reduce duplication when creating URI

--- src/main/java/uk/gov/hmcts/reform/coh/controller/utils/CohUriBuilder.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/utils/CohUriBuilder.java
@@ -4,27 +4,46 @@ import java.util.UUID;
 
 public class CohUriBuilder {
 
+    public static String buildOnlineHearingPost() {
+
+        return "/continuous-online-hearings";
+    }
+
     public static String buildOnlineHearingGet(UUID onlineHearingId) {
 
-        return String.format("/continuous-online-hearings/%s", onlineHearingId);
+        return String.format(buildOnlineHearingPost() + "/%s", onlineHearingId);
+    }
+
+    public static String buildQuestionPost(UUID onlineHearingId) {
+
+        return buildOnlineHearingGet(onlineHearingId) + "/questions";
     }
 
     public static String buildQuestionGet(UUID onlineHearingId, UUID questionId) {
 
-        return String.format("/continuous-online-hearings/%s/questions/%s", onlineHearingId, questionId);
+        return String.format(buildQuestionPost(onlineHearingId) + "/%s", questionId);
     }
 
     public static String buildDecisionGet(UUID onlineHearingId) {
 
-        return String.format("/continuous-online-hearings/%s/decisions", onlineHearingId);
+        return buildOnlineHearingGet(onlineHearingId) + "/decisions";
+    }
+
+    public static String buildAnswerPost(UUID onlineHearingId, UUID questionId) {
+
+        return buildQuestionGet(onlineHearingId, questionId) + "/answers";
     }
 
     public static String buildAnswerGet(UUID onlineHearingId, UUID questionId, UUID answerId) {
 
-        return String.format("/continuous-online-hearings/%s/questions/%s/answers/%s", onlineHearingId, questionId, answerId);
+        return String.format(buildAnswerPost(onlineHearingId, questionId) + "/%s", answerId);
+    }
+
+    public static String buildDecisionReplyPost(UUID onlineHearingId) {
+        return buildOnlineHearingGet(onlineHearingId) + "/decisionreplies/";
     }
 
     public static String buildDecisionReplyGet(UUID onlineHearingId, UUID decisionReplyId) {
-        return String.format("/continuous-online-hearings/%s/decisionreplies/%s", onlineHearingId, decisionReplyId);
+        return String.format(buildDecisionReplyPost(onlineHearingId) + "/%s", decisionReplyId);
     }
 }
