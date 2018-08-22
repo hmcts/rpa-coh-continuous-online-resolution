5367260d6bf6716778f6e13acf5f34ad8a5c1c56
Phillip Denness <dennessphillip@gmail.com>
2018-08-22 16:10:08 +0100

    Fix unit tests

--- src/test/java/uk/gov/hmcts/reform/coh/service/utils/ExpiryCalendarTest.java
+++ src/test/java/uk/gov/hmcts/reform/coh/service/utils/ExpiryCalendarTest.java
@@ -1,6 +1,10 @@
 package uk.gov.hmcts.reform.coh.service.utils;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -9,17 +13,26 @@ import java.util.TimeZone;
 
 import static junit.framework.TestCase.assertEquals;
 
+@RunWith(SpringRunner.class)
 public class ExpiryCalendarTest {
+
+    @InjectMocks
+    private ExpiryCalendar expiryCalendar;
+
+    @Before
+    public void setUp() {
+        expiryCalendar.init();
+    }
 
     @Test
     public void testGetDeadlineReturnsDateOf7DaysAway() {
         Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
-        calendar.add(Calendar.DAY_OF_YEAR, ExpiryCalendar.getInstance().getDeadlineExtensionDays());
+        calendar.add(Calendar.DAY_OF_YEAR, expiryCalendar.getDeadlineExtensionDays());
         calendar.set(Calendar.HOUR_OF_DAY, 23);
         calendar.set(Calendar.MINUTE, 59);
         calendar.set(Calendar.SECOND, 59);
 
-        Date deadlineExpiryDate = ExpiryCalendar.getInstance().getDeadlineExpiryDate();
+        Date deadlineExpiryDate = expiryCalendar.getDeadlineExpiryDate();
         assertEquals(calendar.getTime().toString(), deadlineExpiryDate.toString());
     }
 }
