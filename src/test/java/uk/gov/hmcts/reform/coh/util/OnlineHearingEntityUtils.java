196b44d51cf9bbde20e100bd99bd0a266b5413ef
John Ho <john.ho@solirius.com>
2018-09-10 09:37:13 +0100

    Unused import

--- src/test/java/uk/gov/hmcts/reform/coh/util/OnlineHearingEntityUtils.java
+++ src/test/java/uk/gov/hmcts/reform/coh/util/OnlineHearingEntityUtils.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.coh.util;
 
 import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
-import uk.gov.hmcts.reform.coh.domain.OnlineHearingPanelMember;
 import uk.gov.hmcts.reform.coh.domain.OnlineHearingStateHistory;
 import uk.gov.hmcts.reform.coh.states.OnlineHearingStates;
 
