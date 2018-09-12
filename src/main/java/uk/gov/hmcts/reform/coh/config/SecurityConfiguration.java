9f83de75bf73c376be09ccd64ec53c5dfc4bf083
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-09-12 11:35:08 +0100

    Disable creating sessions for security

--- src/main/java/uk/gov/hmcts/reform/coh/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/coh/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 
 @Configuration
@@ -24,6 +25,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
             .addFilter(filter)
             .authorizeRequests()
             .anyRequest()
