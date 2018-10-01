b331d5ec3cbf764de9ccbcbf04757c9beabdba6f
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-10-01 11:53:41 +0100

    Add static imports

--- src/test/java/uk/gov/hmcts/reform/coh/controller/RelistingControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/coh/controller/RelistingControllerTest.java
@@ -1,14 +1,12 @@
 package uk.gov.hmcts.reform.coh.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,7 +18,10 @@ import uk.gov.hmcts.reform.coh.util.OnlineHearingEntityUtils;
 
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -51,10 +52,10 @@ public class RelistingControllerTest {
     @Test
     public void initiallyReasonIsDraftedAndEmpty() throws Exception {
         String path = "/continuous-online-hearings/" + onlineHearing.getOnlineHearingId() + "/relist";
-        mockMvc.perform(get(path).contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get(path).contentType(APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.reason", Matchers.isEmptyOrNullString()))
-            .andExpect(jsonPath("$.state", Matchers.is("DRAFTED")));
+            .andExpect(jsonPath("$.reason", isEmptyOrNullString()))
+            .andExpect(jsonPath("$.state", is("DRAFTED")));
     }
 
     @Test
@@ -62,7 +63,7 @@ public class RelistingControllerTest {
         String path = "/continuous-online-hearings/" + onlineHearing.getOnlineHearingId() + "/relist";
         RelistingResponse obj = new RelistingResponse("Foo bar", "DRAFTED");
         String request = new ObjectMapper().writeValueAsString(obj);
-        mockMvc.perform(post(path).content(request).contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(post(path).content(request).contentType(APPLICATION_JSON))
             .andExpect(status().isAccepted());
     }
 
@@ -72,12 +73,12 @@ public class RelistingControllerTest {
 
         RelistingResponse obj = new RelistingResponse("Here is a sample reason.", "DRAFTED");
         String request = new ObjectMapper().writeValueAsString(obj);
-        mockMvc.perform(post(path).content(request).contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(post(path).content(request).contentType(APPLICATION_JSON))
             .andExpect(status().isAccepted());
 
-        mockMvc.perform(get(path).contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get(path).contentType(APPLICATION_JSON))
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.reason", Matchers.is(obj.reason)))
-            .andExpect(jsonPath("$.state", Matchers.is(obj.state)));
+            .andExpect(jsonPath("$.reason", is(obj.reason)))
+            .andExpect(jsonPath("$.state", is(obj.state)));
     }
 }
