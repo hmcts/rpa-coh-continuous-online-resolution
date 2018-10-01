ceb80cbb007077d66f3257ef2b65b19c3fa7a2e0
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-10-01 11:28:08 +0100

    Accept relisting request

--- src/main/java/uk/gov/hmcts/reform/coh/controller/RelistingController.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/RelistingController.java
@@ -4,6 +4,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.coh.controller.onlinehearing.RelistingResponse;
@@ -23,5 +25,13 @@ public class RelistingController {
         String state = "DRAFTED";
         RelistingResponse relistingResponse = new RelistingResponse(reason, state);
         return ResponseEntity.ok(relistingResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity createDraft(
+        @PathVariable UUID onlineHearingId,
+        @RequestBody RelistingResponse body
+    ) {
+        return ResponseEntity.accepted().build();
     }
 }
--- src/test/java/uk/gov/hmcts/reform/coh/controller/RelistingControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/coh/controller/RelistingControllerTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.coh.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,10 +13,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.reform.coh.config.WebConfig;
+import uk.gov.hmcts.reform.coh.controller.onlinehearing.RelistingResponse;
 import uk.gov.hmcts.reform.coh.domain.OnlineHearing;
 import uk.gov.hmcts.reform.coh.util.OnlineHearingEntityUtils;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -43,5 +46,14 @@ public class RelistingControllerTest {
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.reason", Matchers.isEmptyString()))
             .andExpect(jsonPath("$.state", Matchers.is("DRAFTED")));
+    }
+
+    @Test
+    public void storingADraftReturnsAccepted() throws Exception {
+        String path = "/continuous-online-hearings/" + onlineHearing.getOnlineHearingId() + "/relist";
+        RelistingResponse obj = new RelistingResponse("Foo bar", "DRAFTED");
+        String request = new ObjectMapper().writeValueAsString(obj);
+        mockMvc.perform(post(path).content(request).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isAccepted());
     }
 }
