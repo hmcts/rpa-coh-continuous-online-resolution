0722e4a56a6a88cb5008d4d7f9810c23f6172d6b
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-10-19 00:38:25 +0100

    Remove unused env variable

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -46,7 +46,6 @@ module "app" {
 
     # idam
     IDAM_API_URL = "${var.idam_api_url}"
-    S2S_BASE_URI = "http://${var.s2s_url}-${local.local_env}.service.core-compute-${local.local_env}.internal"
     S2S_URL = "http://${var.s2s_url}-${local.local_env}.service.core-compute-${local.local_env}.internal"
     S2S_TOKEN = "${data.azurerm_key_vault_secret.s2s_secret.value}"
     S2S_NAMES_WHITELIST = "${var.s2s_names_whitelist}"
