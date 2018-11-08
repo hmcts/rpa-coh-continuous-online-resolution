f8bc626fa3667b10aafb100ca8ffa3d5a1f63fea
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2018-11-08 16:29:49 +0000

    Update stubbed notification endpoints
    As ordered by SSCS

--- src/main/resources/db/changelog/sql/session_event_forwarding_register.sql
+++ src/main/resources/db/changelog/sql/session_event_forwarding_register.sql
@@ -1,39 +1,39 @@
 insert into session_event_forwarding_register(event_type_id, jurisdiction_id, forwarding_endpoint, maximum_retries)
-select 1, 1, 'http://sscs-tya-notif-aat-staging.service.core-compute-aat.internal/coh-send', 3
+select 1, 1, 'http://sscs-tya-notif-aat.service.core-compute-aat.internal/coh-send', 3
 where not exists (select 1 from public.session_event_forwarding_register where event_type_id = 1 and jurisdiction_id = 1);
 
 insert into session_event_forwarding_register(event_type_id, jurisdiction_id, forwarding_endpoint, maximum_retries)
-select 2, 1, 'http://sscs-tya-notif-aat-staging.service.core-compute-aat.internal/coh-send', 3
+select 2, 1, 'http://sscs-tya-notif-aat.service.core-compute-aat.internal/coh-send', 3
 where not exists (select 1 from public.session_event_forwarding_register where event_type_id = 2 and jurisdiction_id = 1);
 
 insert into session_event_forwarding_register(event_type_id, jurisdiction_id, forwarding_endpoint, maximum_retries)
-select 3, 1, 'http://sscs-tya-notif-aat-staging.service.core-compute-aat.internal/coh-send', 3
+select 3, 1, 'http://sscs-tya-notif-aat.service.core-compute-aat.internal/coh-send', 3
 where not exists (select 1 from public.session_event_forwarding_register where event_type_id = 3 and jurisdiction_id = 1);
 
 insert into session_event_forwarding_register(event_type_id, jurisdiction_id, forwarding_endpoint, maximum_retries)
-select 4, 1, 'http://sscs-tya-notif-aat-staging.service.core-compute-aat.internal/coh-send', 3
+select 4, 1, 'http://sscs-tya-notif-aat.service.core-compute-aat.internal/coh-send', 3
 where not exists (select 1 from public.session_event_forwarding_register where event_type_id = 4 and jurisdiction_id = 1);
 
 insert into session_event_forwarding_register(event_type_id, jurisdiction_id, forwarding_endpoint, maximum_retries)
-select 5, 1, 'http://sscs-tya-notif-aat-staging.service.core-compute-aat.internal/coh-send', 3
+select 5, 1, 'http://sscs-tya-notif-aat.service.core-compute-aat.internal/coh-send', 3
 where not exists (select 1 from public.session_event_forwarding_register where event_type_id = 5 and jurisdiction_id = 1);
 
 insert into session_event_forwarding_register(event_type_id, jurisdiction_id, forwarding_endpoint, maximum_retries)
-select 6, 1, 'http://sscs-tya-notif-aat-staging.service.core-compute-aat.internal/coh-send', 3
+select 6, 1, 'http://sscs-tya-notif-aat.service.core-compute-aat.internal/coh-send', 3
 where not exists (select 1 from public.session_event_forwarding_register where event_type_id = 6 and jurisdiction_id = 1);
 
 insert into session_event_forwarding_register(event_type_id, jurisdiction_id, forwarding_endpoint, maximum_retries)
-select 7, 1, 'http://sscs-tya-notif-aat-staging.service.core-compute-aat.internal/coh-send', 3
+select 7, 1, 'http://sscs-tya-notif-aat.service.core-compute-aat.internal/coh-send', 3
 where not exists (select 1 from public.session_event_forwarding_register where event_type_id = 7 and jurisdiction_id = 1);
 
 insert into session_event_forwarding_register(event_type_id, jurisdiction_id, forwarding_endpoint, maximum_retries)
-select 8, 1, 'http://sscs-tya-notif-aat-staging.service.core-compute-aat.internal/coh-send', 3
+select 8, 1, 'http://sscs-tya-notif-aat.service.core-compute-aat.internal/coh-send', 3
 where not exists (select 1 from public.session_event_forwarding_register where event_type_id = 8 and jurisdiction_id = 1);
 
 insert into session_event_forwarding_register(event_type_id, jurisdiction_id, forwarding_endpoint, maximum_retries)
-select 9, 1, 'http://sscs-tya-notif-aat-staging.service.core-compute-aat.internal/coh-send', 3
+select 9, 1, 'http://sscs-tya-notif-aat.service.core-compute-aat.internal/coh-send', 3
 where not exists (select 1 from public.session_event_forwarding_register where event_type_id = 9 and jurisdiction_id = 1);
 
 insert into session_event_forwarding_register(event_type_id, jurisdiction_id, forwarding_endpoint, maximum_retries)
-select 10, 1, 'http://sscs-tya-notif-aat-staging.service.core-compute-aat.internal/coh-send', 3
+select 10, 1, 'http://sscs-tya-notif-aat.service.core-compute-aat.internal/coh-send', 3
 where not exists (select 1 from public.session_event_forwarding_register where event_type_id = 10 and jurisdiction_id = 1);
