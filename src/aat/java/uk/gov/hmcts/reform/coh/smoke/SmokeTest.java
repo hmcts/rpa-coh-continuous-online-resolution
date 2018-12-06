e3300153a4ae82e4c9f857c92c43a0f12914960d
Phillip Denness <dennessphillip@gmail.com>
2018-12-06 14:23:06 +0000

    Check health endpoint in smoke test (#235)
    * Check health endpoint in smoke test
    
    * Remove dummy smoke test

--- src/aat/java/uk/gov/hmcts/reform/coh/smoke/SmokeTest.java
+++ src/aat/java/uk/gov/hmcts/reform/coh/smoke/SmokeTest.java
@@ -1,13 +1,42 @@
 package uk.gov.hmcts.reform.coh.smoke;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import uk.gov.hmcts.reform.coh.functional.bdd.utils.TestTrustManager;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+@ContextConfiguration
+@SpringBootTest
+@ActiveProfiles("cucumber")
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 public class SmokeTest {
 
+    private RestTemplate restTemplate;
+
+    @Value("${base-urls.test-url}")
+    protected String baseUrl;
+
     @Test
-    public void should_be_true() {
-        assertTrue(true);
+    public void testAppIsUp() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
+        restTemplate = new RestTemplate(TestTrustManager.getInstance().getTestRequestFactory());
+        ResponseEntity<Health> response =restTemplate.getForEntity(baseUrl + "/health", Health.class);
+        Health health = response.getBody();
+
+        assertEquals("UP", health.getStatus());
     }
 }
