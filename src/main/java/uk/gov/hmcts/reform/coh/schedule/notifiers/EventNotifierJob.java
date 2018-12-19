60091bd923583b368a5f3f0dea053cb7a67032a9
johnhosolirius <38843249+johnhosolirius@users.noreply.github.com>
2018-12-19 10:58:14 +0000

    Fix sonar issues02 (#245)
    * Sonar complaint:serializable
    
    * Sonar complaint:serializable
    
    * Sonar complaint:serializable
    
    * Sonar complaints: override
    
    * SOnar complaints: private constructor
    
    * Sonar complaint: parameter
    
    * Sonar complaint: parameter
    
    * Sonar complaint: parameter
    
    * Sonar complaint: parameter

--- src/main/java/uk/gov/hmcts/reform/coh/schedule/notifiers/EventNotifierJob.java
+++ src/main/java/uk/gov/hmcts/reform/coh/schedule/notifiers/EventNotifierJob.java
@@ -59,7 +59,7 @@ public class EventNotifierJob {
     public void execute() {
 
         List<SessionEvent> sessionEvents = getPendingSessionEvents();
-        log.debug("Pending session events found: " + sessionEvents.size());
+        log.debug("Pending session events found: {}", sessionEvents.size());
         for (SessionEvent sessionEvent : sessionEvents) {
             log.info("Processing session event: {}", sessionEvent.getEventId());
 
