b4349360567b06dbcebca8d5dadfea99b78438ac
Phillip Denness <dennessphillip@gmail.com>
2018-12-21 14:14:44 +0000

    use restassured

--- src/aat/java/uk/gov/hmcts/reform/coh/smoke/SmokeTest.java
+++ src/aat/java/uk/gov/hmcts/reform/coh/smoke/SmokeTest.java
@@ -1,23 +1,15 @@
 package uk.gov.hmcts.reform.coh.smoke;
 
+import io.restassured.RestAssured;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
-import uk.gov.hmcts.reform.coh.functional.bdd.utils.TestTrustManager;
-
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 @ContextConfiguration
 @SpringBootTest
@@ -32,11 +24,11 @@ public class SmokeTest {
     protected String baseUrl;
 
     @Test
-    public void testAppIsUp() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-        restTemplate = new RestTemplate(TestTrustManager.getInstance().getTestRequestFactory());
-        ResponseEntity<Health> response =restTemplate.getForEntity(baseUrl + "/health", Health.class);
-        Health health = response.getBody();
+    public void testAppIsUp() {
 
-        assertEquals("UP", health.getStatus());
+        RestAssured.given().relaxedHTTPSValidation().get(baseUrl + "/health").then()
+                .statusCode(200).assertThat();
+
+        // TODO add body search for UP
     }
 }
