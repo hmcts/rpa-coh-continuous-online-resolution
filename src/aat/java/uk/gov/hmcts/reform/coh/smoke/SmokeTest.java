f70dd83ad1e286ac1aab04337bd6b8652d361db5
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2019-02-01 08:45:38 +0000

    Refactor smoke test to use RestAssured

--- src/aat/java/uk/gov/hmcts/reform/coh/smoke/SmokeTest.java
+++ src/aat/java/uk/gov/hmcts/reform/coh/smoke/SmokeTest.java
@@ -1,23 +1,16 @@
 package uk.gov.hmcts.reform.coh.smoke;
 
+import io.restassured.RestAssured;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
-import uk.gov.hmcts.reform.coh.functional.bdd.utils.TestTrustManager;
 
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.Matchers.equalTo;
 
 @ContextConfiguration
 @SpringBootTest
@@ -26,17 +19,17 @@ import static org.junit.Assert.assertTrue;
 @AutoConfigureMockMvc
 public class SmokeTest {
 
-    private RestTemplate restTemplate;
-
     @Value("${base-urls.test-url}")
     protected String baseUrl;
 
     @Test
-    public void testAppIsUp() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-        restTemplate = new RestTemplate(TestTrustManager.getInstance().getTestRequestFactory());
-        ResponseEntity<Health> response =restTemplate.getForEntity(baseUrl + "/health", Health.class);
-        Health health = response.getBody();
-
-        assertEquals("UP", health.getStatus());
+    public void testAppIsUp() {
+        RestAssured
+            .given()
+            .relaxedHTTPSValidation()
+            .baseUri(baseUrl)
+            .get("/health")
+            .then()
+            .body("status", equalTo("UP"));
     }
 }
