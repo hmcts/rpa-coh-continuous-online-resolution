4bf467fcab5612b92a31c4897f8d445bac620232
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2019-02-11 11:39:05 +0000

    Remove requestedText from unit tests

--- src/test/java/uk/gov/hmcts/reform/coh/controller/LoggingAnswerUpdateErrorsTest.java
+++ src/test/java/uk/gov/hmcts/reform/coh/controller/LoggingAnswerUpdateErrorsTest.java
@@ -234,6 +234,6 @@ public class LoggingAnswerUpdateErrorsTest {
         assertThat(trackedEventProperties.getValue())
             .containsEntry("onlineHearingId", onlineHearingId)
             .containsEntry("answerId", answer.getAnswerId().toString())
-            .containsKeys("requestedText", "requestedState", "error");
+            .containsKeys("requestedState", "error");
     }
 }
