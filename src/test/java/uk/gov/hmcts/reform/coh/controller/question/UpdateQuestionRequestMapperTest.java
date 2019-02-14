271970361b711676462428ee67023fca9c1c51cd
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2019-02-14 10:04:21 +0000

    Add test cases for UpdateQuestionRequestMapper (#268)
    * Add test cases for UpdateQuestionRequestMapper
    
    JIRA: COH-192
    
    * Refactor UpdateQuestionRequestMapperTest
    
    Use setUp() method to prepare objects used in each test.
    
    * Use predefined UUIDs to check linked questions

--- src/test/java/uk/gov/hmcts/reform/coh/controller/question/UpdateQuestionRequestMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/coh/controller/question/UpdateQuestionRequestMapperTest.java
@@ -1,19 +1,70 @@
 package uk.gov.hmcts.reform.coh.controller.question;
 
+import com.google.common.collect.ImmutableSet;
+
+import org.junit.Before;
 import org.junit.Test;
 import uk.gov.hmcts.reform.coh.domain.Question;
+
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UpdateQuestionRequestMapperTest {
 
+    private UpdateQuestionRequest request;
+    private Question question;
+
+    @Before
+    public void setUp() {
+        request = new UpdateQuestionRequest();
+        request.setQuestionRound("1");
+        question = new Question();
+    }
+
+    @Test
+    public void maps_question_text() {
+        request.setQuestionBodyText("Requested");
+        question.setQuestionText("Current");
+
+        UpdateQuestionRequestMapper.map(question, request);
+
+        assertThat(question.getQuestionText()).isEqualTo(request.getQuestionBodyText());
+    }
+
+    @Test
+    public void maps_question_header() {
+        request.setQuestionHeaderText("Requested");
+        question.setQuestionHeaderText("Current");
+
+        UpdateQuestionRequestMapper.map(question, request);
+
+        assertThat(question.getQuestionHeaderText()).isEqualTo(request.getQuestionHeaderText());
+    }
+
+    @Test
+    public void maps_question_round() {
+        request.setQuestionRound("1");
+        question.setQuestionOrdinal(0);
+
+        UpdateQuestionRequestMapper.map(question, request);
+
+        assertThat(question.getQuestionOrdinal()).isEqualTo(Integer.parseInt(request.getQuestionRound()));
+    }
+
+    @Test
+    public void maps_linked_question_id() {
+        request.setLinkedQuestionId(ImmutableSet.of(UUID.fromString("70e81346-df7b-420a-bd8e-419825d10a27")));
+        question.setLinkedQuestions(ImmutableSet.of(UUID.fromString("deee64ad-7cd1-4904-ab93-3561f842f2ba")));
+
+        UpdateQuestionRequestMapper.map(question, request);
+
+        assertThat(question.getLinkedQuestions()).containsExactlyInAnyOrderElementsOf(request.getLinkedQuestionId());
+    }
+
     @Test
     public void maps_owner_id() {
-        UpdateQuestionRequest request = new UpdateQuestionRequest();
-        request.setQuestionRound("1");
         request.setOwnerReference("The Owner");
-
-        Question question = new Question();
         question.setOwnerReferenceId("To be updated");
 
         UpdateQuestionRequestMapper.map(question, request);
