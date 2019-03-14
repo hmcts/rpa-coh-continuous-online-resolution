00979a4ed8620f75fee8917ef76905da8b4e2467
Karol Jastrzębski <karol.jastrzebski@gmail.com>
2019-03-14 15:00:24 +0000

    Read client service secret from SSCS

---
+++ infrastructure/testing.tf
@@ -0,0 +1,19 @@
+locals {
+  sscsVaultName = "sscs-${local.local_env}"
+}
+
+data "azurerm_key_vault" "sscs_key_vault" {
+  name                = "${local.sscsVaultName}"
+  resource_group_name = "${local.sscsVaultName}"
+}
+
+data "azurerm_key_vault_secret" "idam-sscs-oauth2-client-secret" {
+  name      = "idam-sscs-oauth2-client-secret"
+  vault_uri = "${data.azurerm_key_vault.sscs_key_vault.vault_uri}"
+}
+
+resource "azurerm_key_vault_secret" "idam-sscs-secret" {
+  name = "idam-sscs-oauth2-client-secret"
+  value = "${data.azurerm_key_vault_secret.idam-sscs-oauth2-client-secret}"
+  vault_uri = "${module.local_key_vault.key_vault_uri}"
+}
