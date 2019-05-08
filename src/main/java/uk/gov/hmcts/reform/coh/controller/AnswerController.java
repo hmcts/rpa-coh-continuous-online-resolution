c3ca5a5820a7bad30ca73b130c7e88cc44d25a78
johnhosolirius <john.ho@solirius.com>
2019-05-08 20:19:42 +0100

    Hopefully fix sonar complaint about XSS attack

--- src/main/java/uk/gov/hmcts/reform/coh/controller/AnswerController.java
+++ src/main/java/uk/gov/hmcts/reform/coh/controller/AnswerController.java
@@ -45,6 +45,7 @@ import uk.gov.hmcts.reform.coh.states.AnswerStates;
 import uk.gov.hmcts.reform.coh.states.OnlineHearingStates;
 import uk.gov.hmcts.reform.coh.states.QuestionStates;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.Arrays;
@@ -95,7 +96,7 @@ public class AnswerController {
             @ApiResponse(code = 422, message = "Validation error")
     })
     @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity createAnswer(UriComponentsBuilder uriBuilder, @PathVariable UUID onlineHearingId, @PathVariable UUID questionId, @RequestBody AnswerRequest request) {
+    public ResponseEntity createAnswer(UriComponentsBuilder uriBuilder, @PathVariable UUID onlineHearingId, @PathVariable UUID questionId, @RequestBody AnswerRequest request) throws UnsupportedEncodingException {
 
         Optional<OnlineHearing> optionalOnlineHearing = onlineHearingService.retrieveOnlineHearing(
                 UUID.fromString(URLDecoder.decode(onlineHearingId.toString())));
@@ -153,7 +154,12 @@ public class AnswerController {
         UriComponents uriComponents =
                 uriBuilder.path(
                         CohUriBuilder.buildAnswerGet(
-                                UUID.fromString(URLEncoder.encode(optionalOnlineHearing.get().getOnlineHearingId().toString())),
+                                UUID.fromString(
+                                        URLEncoder.encode(
+                                                optionalOnlineHearing.get().getOnlineHearingId().toString(),
+                                                java.nio.charset.StandardCharsets.UTF_8.toString()
+                                        )
+                                ),
                                 optionalQuestion.get().getQuestionId(),
                                 answerResponse.getAnswerId()
                         )
