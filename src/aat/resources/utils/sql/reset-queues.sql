cf71d2c96321aa111ed66ffb02e28490db81a4e4
benouaer <tech@benouaer.com>
2019-07-23 15:03:44 +0100

    update sql script to replicate reset queue api call

--- src/aat/resources/utils/sql/reset-queues.sql
+++ src/aat/resources/utils/sql/reset-queues.sql
@@ -1,5 +1,6 @@
 
-UPDATE public.session_event SET retries=0
+UPDATE public.session_event
+SET retries=0, forwarding_state_id=(SELECT forwarding_state_id FROM public.session_event_forwarding_state WHERE forwarding_state_name IN ('event_forwarding_pending'))
 WHERE EVNT_ID IN (SELECT event_type_id FROM public.session_event_type WHERE event_type_name IN ('question_round_issued', 'decision_issued'))
-  AND JURS_ID=(SELECT jurisdiction_id FROM public.jurisdiction WHERE jurisdiction_name='SSCS')
-  AND forwarding_state_id IN (SELECT forwarding_state_id FROM public.session_event_forwarding_state WHERE forwarding_state_name IN ('event_forwarding_pending', 'event_forwarding_failed'));
+AND JURS_ID=(SELECT jurisdiction_id FROM public.jurisdiction WHERE jurisdiction_name='SSCS')
+AND forwarding_state_id IN (SELECT forwarding_state_id FROM public.session_event_forwarding_state WHERE forwarding_state_name IN ('event_forwarding_pending', 'event_forwarding_failed'));
