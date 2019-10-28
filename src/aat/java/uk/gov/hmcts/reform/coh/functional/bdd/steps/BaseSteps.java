659574126209d84898f47be391d49180bbb022d4
Damon Green <damon.green@btinternet.com>
2019-10-28 16:30:57 +0000

    Revert "Merge branch 'master' into add_logging"
    This reverts commit a9c3f10c3fb5823e437f7db4c605ce783330111b, reversing
    changes made to fe83674d419911fbfb2caad120866c256d070036.

--- src/aat/java/uk/gov/hmcts/reform/coh/functional/bdd/steps/BaseSteps.java
+++ src/aat/java/uk/gov/hmcts/reform/coh/functional/bdd/steps/BaseSteps.java
@@ -66,6 +66,9 @@ public class BaseSteps {
 
         Optional.ofNullable(testContext.getHttpContext().getIdamServiceRef())
             .ifPresent(token -> header.add(ServiceRequestAuthorizer.AUTHORISATION, token));
+
+        System.out.println(String.format("Headers: \n%s", header.toString()));
+
     }
 
     private void prepareAuthenticationTokens() {
